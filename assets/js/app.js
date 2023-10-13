fccc2bb484ae205a5fbb74a01d00b5a54eeee725
Komiljon <gulnozarahmatova21@gmail.com>
2023-10-13 17:51:58 +0500

    nnnn

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 let registrationBtnEl = document.querySelector(".advert__leader-registration");
-let registrationBtnCloneEl = document.querySelector(".advert__leader-registration-clone");
 let popupEl = document.querySelector("#advert__popup");
 let overlayEl = document.querySelector("#advert__overlay");
 let popupSubmitBtn = document.getElementById("popup__submit");
 let overlayCancelBtn = document.getElementById("overlay_cancel");
+let overlayCancelLayer = document.querySelector(".advert__main-overlay");
 let popupFormEl = document.getElementById("popp_id");
 let nameInputEl = document.querySelector(".advert__main-name-input");
 let numInpuEl = document.querySelector(".advert__main-number-input");
@@ -13,15 +13,7 @@ let numHrEl = document.querySelector(".advert__input-underline-second");
 const alphs = /^[a-zA-Z]*$/;
 
 registrationBtnEl.addEventListener("click", () => {
-  if ((popupEl.style.visibility = "hidden")) {
-    popupEl.style.visibility = "visible";
-    overlayEl.style.opacity = "1";
-    overlayEl.style.pointerEvents = "all";
-  }
-});
-
-registrationBtnCloneEl.addEventListener("click", () => {
-  if ((popupEl.style.visibility = "hidden")) {
+  if (popupEl.style.visibility = "hidden") {
     popupEl.style.visibility = "visible";
     overlayEl.style.opacity = "1";
     overlayEl.style.pointerEvents = "all";
@@ -29,7 +21,15 @@ registrationBtnCloneEl.addEventListener("click", () => {
 });
 
 overlayCancelBtn.addEventListener("click", () => {
-  if ((popupEl.style.visibility = "visible")) {
+  if (popupEl.style.visibility = "visible") {
+    popupEl.style.visibility = "hidden";
+    overlayEl.style.opacity = "0";
+    overlayEl.style.pointerEvents = "none";
+  }
+});
+
+overlayCancelLayer.addEventListener("click", () => {
+  if (popupEl.style.visibility = "visible") {
     popupEl.style.visibility = "hidden";
     overlayEl.style.opacity = "0";
     overlayEl.style.pointerEvents = "none";
@@ -38,19 +38,20 @@ overlayCancelBtn.addEventListener("click", () => {
 
 popupFormEl.addEventListener("submit", (e) => {
   e.preventDefault();
-  // if ((nameInputEl.length <= 2) && (numInpuEl.value = "")) {
-  //   console.log("alerta4");
-  // } 
-  if (nameInputEl.value.length <= 2) {
-    console.log("alerta1");
-    nameHrEl.style.backgroundColor = "red";
-    nameInputEl.style.border = "none";
-  } 
-  else if (numInpuEl.value.length < 9) {
-    console.log("alerta3");
-    nameHrEl.style.backgroundColor = "#a4a4a4";
+  if ((nameInputEl.value = ' ') && (numInpuEl.value = ' ')) {
     numHrEl.style.backgroundColor = "red";
+    nameHrEl.style.backgroundColor = "red";
   } 
+  // else if(nameInputEl.value.length <= 2) {
+  //   console.log("alerta1");
+  //   nameHrEl.style.backgroundColor = "red";
+  //   nameInputEl.style.border = "none";
+  // } 
+  // else if (numInpuEl.value.length < 9) {
+  //   console.log("alerta3");
+  //   nameHrEl.style.backgroundColor = "#a4a4a4";
+  //   numHrEl.style.backgroundColor = "red";
+  // } 
   else {
     popupEl.style.visibility = "hidden";
     overlayEl.style.opacity = "0";
@@ -61,3 +62,4 @@ popupFormEl.addEventListener("submit", (e) => {
     nameInputEl.value = "";
   }
 });
+
