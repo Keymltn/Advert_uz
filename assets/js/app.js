dfe1d5791491e33885fb39e3f5c9bee3d101177b
Komiljon <gulnozarahmatova21@gmail.com>
2023-10-15 20:16:46 +0500

    finished the project

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,9 @@ let nameInputEl = document.querySelector(".advert__main-name-input");
 let numInpuEl = document.querySelector(".advert__main-number-input");
 let nameHrEl = document.querySelector(".advert__input-underline-frst");
 let numHrEl = document.querySelector(".advert__input-underline-second");
-
-const alphs = /^[a-zA-Z]*$/;
+let topPopupEl1 = document.querySelector(".advert__main-top-popup-first");
+let topPopupEl2 = document.querySelector(".advert__main-top-popup-second");
+let countDownEl = document.querySelector(".advert__place-left-p")
 
 registrationBtnEl.addEventListener("click", () => {
   if (popupEl.style.visibility = "hidden") {
@@ -38,28 +39,53 @@ overlayCancelLayer.addEventListener("click", () => {
 
 popupFormEl.addEventListener("submit", (e) => {
   e.preventDefault();
-  if ((nameInputEl.value = ' ') && (numInpuEl.value = ' ')) {
+  if (nameInputEl.value === '' && numInpuEl.value === '') {
     numHrEl.style.backgroundColor = "red";
     nameHrEl.style.backgroundColor = "red";
-  } 
-  // else if(nameInputEl.value.length <= 2) {
-  //   console.log("alerta1");
-  //   nameHrEl.style.backgroundColor = "red";
-  //   nameInputEl.style.border = "none";
-  // } 
-  // else if (numInpuEl.value.length < 9) {
-  //   console.log("alerta3");
-  //   nameHrEl.style.backgroundColor = "#a4a4a4";
-  //   numHrEl.style.backgroundColor = "red";
-  // } 
+  }
+  else if (nameInputEl.value.length <= 2) {
+    nameHrEl.style.backgroundColor = "red";
+    nameInputEl.style.border = "none";
+  }
+  else if (numInpuEl.value.length < 9) {
+    nameHrEl.style.backgroundColor = "#a4a4a4";
+    numHrEl.style.backgroundColor = "red";
+  }
   else {
     popupEl.style.visibility = "hidden";
     overlayEl.style.opacity = "0";
     overlayEl.style.pointerEvents = "none";
-    nameHrEl.style.backgroundColor = "#a4a4a4";
-    numHrEl.style.backgroundColor = "#a4a4a4";
     numInpuEl.value = "";
     nameInputEl.value = "";
+    nameHrEl.style.backgroundColor = "#a4a4a4";
+    numHrEl.style.backgroundColor = "#a4a4a4";
   }
 });
 
+setTimeout(() => {
+  if (topPopupEl1.style.top = '-25%') {
+    topPopupEl1.style.top = '5%';
+    countDownEl.innerHTML = 'Joylar soni: 15 ta'
+  }
+}, 6000);
+
+setTimeout(() => {
+  if (topPopupEl1.style.top = '5%') {
+    topPopupEl1.style.top = '-25%';
+    return false
+  }
+}, 9000);
+
+setTimeout(() => {
+  if (topPopupEl2.style.top = '-25%') {
+    topPopupEl2.style.top = '5%';
+    countDownEl.innerHTML = 'Joylar soni: 14 ta'
+  }
+}, 13000);
+
+setTimeout(() => {
+  if (topPopupEl2.style.top = '5%') {
+    topPopupEl2.style.top = '-25%';
+    return false
+  }
+}, 16000);
